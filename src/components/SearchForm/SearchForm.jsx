20e94a059f4933d196ac1545abfcc1a022985853
Max Rohivskyi <maxrogivsky@gmail.com>
2022-10-29 13:03:53 +0300

    add-task1-gallery

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,35 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  hendlerInput = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+  handlerSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+
+    this.setState({ query: '' });
+  };
   render() {
-    return <h2>SearchForm</h2>;
+    const { query } = this.state;
+    return (
+      <SearchFormStyled onSubmit={this.handlerSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          value={query}
+          onChange={this.hendlerInput}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,12 +2,95 @@ import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { MdStarHalf } from 'react-icons/md';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    isVisible: false,
+    error: null,
+    isLoading: false,
+    isEmpty: false,
+  };
+  onHandlerSubmit = value => {
+    this.setState({ query: value, error: null });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getPhotos(query, page);
+    }
+  }
+  getPhotos = async (query, page) => {
+    if (!query) {
+      return;
+    }
+    this.setState({ isLoading: true });
+
+    try {
+      const {
+        photos,
+        per_page,
+        total_result,
+        page: currentPage,
+      } = await ImageService.getImages(query, page);
+      console.log(photos);
+
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+
+      if (photos.length === 0) {
+        this.setState({
+          images: [],
+          isVisible: false,
+        });
+      }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        isVisible: photos.length === 0 ? false : true,
+      }));
+    } catch (error) {
+      console.log(error);
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
   render() {
+    const { isVisible, images, isLoading, error, isEmpty } = this.state;
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.onHandlerSubmit} />
+        {isEmpty && (
+          <Text textAlign="center">❌ We do not find images, try again</Text>
+        )}
+        {error && <Text textAlign="center">❌ Something go wrong...</Text>}
+        <Grid>
+          {images.length > 0 &&
+            images.map(({ id, avg_color, alt, src }) => (
+              <GridItem key={id}>
+                <CardItem>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            ))}
+        </Grid>
+        {isVisible && (
+          <Button onClick={this.onLoadMore} type="button">
+            {isLoading ? 'Loading...' : 'Load More'}
+          </Button>
+        )}
+
+        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
       </>
     );
   }
